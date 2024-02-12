c43cd8db10a63a65b061d13e434eaa02c73f9028
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-02-12 23:28:53 +0800

    update

--- customize/diy/functions.sh
+++ customize/diy/functions.sh
@@ -32,26 +32,25 @@ send_message() {
 }
 
 check_compile() {
-    if ls bin/targets/*/*/ | grep -q '\.img\.gz$'; then
-        (
-            cd bin/targets/*/*/
-            echo "BD_COMPILE=success" >> $GITHUB_ENV
-            echo "FIRMWARE_PATH=$PWD" >> $GITHUB_ENV
-            echo "DATE=$(date +"%Y.%m.%d")" >> $GITHUB_ENV
-            echo "KERNEL=$(cat *.manifest | grep ^kernel | cut -d- -f2 | tr -d ' ')" >> $GITHUB_ENV
-            uppercase_string=$(echo "${{ env.BD_PROJECT }}" | tr '[:lower:]' '[:upper:]')
-            echo "Uppercase_String=$uppercase_string" >> $GITHUB_ENV
-            tar -czvf packages.tar.gz packages
-            rm -rf packages
-            echo "===========编译成功,准备上传==========="
-            send_message "编译成功,准备上传" "编译成功,准备上传"
-        )
-        break
+    if ls $GITHUB_WORKSPACE/${{ env.BD_PROJECT }}/bin/targets/*/*/ | grep -q '\.img\.gz$'; then
+    cd $GITHUB_WORKSPACE/${{ env.BD_PROJECT }}/bin/targets/*/*/
+    echo "BD_COMPILE=success" >> $GITHUB_ENV
+    echo "FIRMWARE_PATH=$PWD" >> $GITHUB_ENV
+    echo "DATE=$(date +"%Y.%m.%d")" >> $GITHUB_ENV
+    echo "KERNEL=$(cat *.manifest | grep ^kernel | cut -d- -f2 | tr -d ' ')" >> $GITHUB_ENV
+    uppercase_string=$(echo "${{ env.BD_PROJECT }}" | tr '[:lower:]' '[:upper:]')
+    echo "Uppercase_String=$uppercase_string" >> $GITHUB_ENV
+    tar -czvf packages.tar.gz packages
+    rm -rf packages
+    echo "===========编译成功,准备上传==========="
+    send_message "编译成功,准备上传" "编译成功,准备上传"
+    break
     else
-        rm -f /home/runner/stop_signal
-        echo "===========编译失败,继续暂停==========="
-        echo "BD_COMPILE=failure" >> $GITHUB_ENV
-        send_message "编译失败,继续暂停" "编译失败,继续暂停"
+    cd $GITHUB_WORKSPACE/${{ env.BD_PROJECT }}
+    rm -f /home/runner/stop_signal
+    echo "===========编译失败,继续暂停==========="
+    echo "BD_COMPILE=failure" >> $GITHUB_ENV
+    send_message "编译失败,继续暂停" "编译失败,继续暂停"
     fi
 }
 
