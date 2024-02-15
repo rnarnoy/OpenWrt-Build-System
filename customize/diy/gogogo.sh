6a68a1d578b54a6334e5248312e616ab9d5880a8
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-02-16 01:01:11 +0800

    update

--- customize/diy/gogogo.sh
+++ customize/diy/gogogo.sh
@@ -2,11 +2,11 @@ if ! command -v gogogo > /dev/null; then
     echo '#!/bin/bash' > gogogo
     echo 'sudo touch /home/runner/stop_signal' >> gogogo
     sudo chmod a+x gogogo && sudo cp gogogo /usr/bin/
+    echo "================================================"
     echo "================gogogo命令已加载!================"
-    echo "                                                "
-    echo "                                                "
+    echo "================================================"
 else
+    echo "================================================"
     echo "================gogogo命令已存在!================"
-    echo "                                                "
-    echo "                                                "
+    echo "================================================"
 fi
