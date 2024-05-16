137f9d75ba655a79a3a3080ccaf81fc7af2112a2
rnarnoy <167851414+rnarnoy@users.noreply.github.com>
2024-05-17 04:24:40 +0800

    Add files via upload

--- customize/immortalwrt_x86_64.sh
+++ customize/immortalwrt_x86_64.sh
@@ -111,10 +111,10 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2dat
 ##########################################其他设置##########################################
 
 # 修改默认登录地址
-sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.99.1/g' ./package/base-files/files/bin/config_generate
 
 # 修改默认登录密码
-#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' ./package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$eRZDGn.w$lAHe0nuYvaem61CpArhxV.:0:0:99999:7:::/g' ./package/base-files/files/etc/shadow
 
 
 
